c298aa71b14df982b9422e801b3635a17656883d
Sourav <souravmahato601@gmail.com>
2025-08-22 02:40:33 +0530

    added edit feature

--- src/components/Noteitem.jsx
+++ src/components/Noteitem.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import NoteContext from "../context/notes/NotesContext";
 
 export default function Noteitem(props) {
-  const {note}=props;
+  const {note, updatenote}=props;
   const context= useContext(NoteContext);
   const {deleteNote, editNote}=context;
   return (
@@ -10,9 +10,9 @@ export default function Noteitem(props) {
       <div className="card h-100">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
-          <div className="d-flex">
+          <div className="d-flex"> 
             <div className="fs-4"><i className="ri-delete-bin-fill" onClick={()=>{deleteNote(note._id)}}></i></div>
-            <div className="mx-2 fs-4"><i className="ri-edit-box-fill" onClick={()=>{editNote(note._id,note.title,note.description,note.tag)}}></i></div>
+            <div className="mx-2 fs-4"><i className="ri-edit-box-fill" onClick={()=>{updatenote(note)}}></i></div>
           </div>
           <p className="card-text">{note.description}</p>
         </div>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import NoteContext from "../context/notes/NotesContext";
 import Noteitem from "./Noteitem";
 export default function Notes() {
@@ -7,12 +7,59 @@ export default function Notes() {
     useEffect(()=>{
         getNotes()
     },[])
+    const ref=useRef(null)
+    const updateNote=(currnote)=>{
+        ref.current.click();
+        setNote(currnote);
+    }
+    
+    const [enote,setNote]=useState({title:"",description:"",tag:""})
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        addNote(enote.title,enote.description,enote.tag);
+    }
+    const handleChange=(e)=>{
+        setNote({...enote,[e.target.name]:e.target.value})
+    }
+
     return (
         <>
+        <button type="button" ref={ref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+        </button>
+        <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Note</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                <div className="mb-3">
+                <label htmlFor="title" className="form-label">Title</label>
+                <input type="text" value={enote.title} className="form-control" id="etitle" name="etitle" onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                <label htmlFor="edescription" className="form-label">Description</label>
+                <input type="text" value={enote.description} className="form-control" id="edescription" name="edescription" onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                <label htmlFor="etag" className="form-label">Tag</label>
+                <input type="text"value={enote.tag} className="form-control" id="etag" name="etag" onChange={handleChange}/>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary" onClick={handleSubmit}>Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
         <div className="container">
             <h1>You Notes</h1>
             <div className="row">{notes.map((note)=>{
-                return(<Noteitem key={note._id} note={note}/>)
+                return(<Noteitem key={note._id} updatenote={updateNote} note={note}/>)
             })}</div>
         </div>
         </>
